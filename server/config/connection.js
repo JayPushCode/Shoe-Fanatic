babc9ed2948cb954b6f52fc3e3cfec1dc14a470c
JayPushCode <JayPushCode@gmail.com>
2025-06-20 02:47:58 -0400

    Converting connection to use env file and uploading example

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,22 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
+
 mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb+srv://jacobnordan:Jacob123@cluster0.vxby4no.mongodb.net/?retryWrites=true&w=majority",
+  process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   }
-);
+)
+.then(() => console.log('MongoDB connected'))
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+});
+
+
 
 module.exports = mongoose.connection;
