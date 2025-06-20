aedcc748ac5f9c6333b5cedaf847c69d0f4d76a5
JayPushCode <JayPushCode@gmail.com>
2025-06-20 10:35:24 -0400

    script and proxy cleanup

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect(
-  process.env.MONGODB_URI,
+mongoose.connect(uri,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
